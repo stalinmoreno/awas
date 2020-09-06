f3851f8d74fbb4b50be5e10c04b810e59bf5c538
StalinMoreno <mpstalinelias@gmail.com>
2020-09-06 18:38:04 -0500

    rutas

--- js/login.js
+++ js/login.js
@@ -3,13 +3,13 @@
 var register = function (e) {
   e.preventDefault();
   console.log(window.location);
-  window.location = window.location.origin + '/views/register.html';
+  window.location = window.location.href + '/views/register.html';
 };
 
 var login = function (e) {
   e.preventDefault();
   console.log(window.location);
-  window.location = window.location.origin + '/views/personality.html';
+  window.location = window.location.href + '/views/personality.html';
 };
 
 document.getElementById('btnRegister').addEventListener("click", register);
